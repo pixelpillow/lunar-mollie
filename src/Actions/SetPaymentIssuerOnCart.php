f8fba8a2f8493d52710c3e96d6c8cfd706570b69
Thomas van der Westen <thomas@pixelpillow.nl>
2023-07-18 14:41:41 +0200

    Remove optional argument

--- src/Actions/SetPaymentIssuerOnCart.php
+++ src/Actions/SetPaymentIssuerOnCart.php
@@ -9,15 +9,11 @@ class SetPaymentIssuerOnCart
     /**
      * Set the payment issuer on the cart
      *
-     * @param  null|Cart  $cart The cart to set the payment issuer on.
+     * @param  Cart  $cart The cart to set the payment issuer on.
      * @param  string  $paymentIssuer The payment issuer to set. e.g. ideal_ABNANL2A
      */
-    public function __invoke(?Cart $cart, string $paymentIssuer): void
+    public function __invoke(Cart $cart, string $paymentIssuer): void
     {
-        if (! $cart) {
-            return;
-        }
-
         $meta = (array) $cart->meta;
         $meta['payment_issuer'] = $paymentIssuer;
 
